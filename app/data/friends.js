72ef19c945957e08251b5a5ccb0fa659210d9284
Julian Hicks <jkballin@gmail.com>
2019-04-26 18:36:10 -0700

    fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 const friendsArray = [
-  {
-    name: `Jane`,
-    picture: `https://randomuser.me/api/portraits/women/76.jpg`,
-    answers: [`5`, `3`, `1`, `5`, `1`, `5`, `4`, `5`, `4`, `3`]
+  {// CHange to Black Panther Characters T'Challa Nakia Killmonger and Okoye
+    name: `Nakia`,
+    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq57adiO_hAhUXsJ4KHWquAWQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.zimbio.com%2FThe%2BStars%2Bof%2B%27Black%2BPanther%27%2BIn%2Band%2BOut%2Bof%2BCostume%2Farticles%2F1n2sZ_pd4Gf%2FNakia&psig=AOvVaw00apdAJS0iW5F0s_GpDGt-&ust=1556412941417744`,
+    answers: [`2`, `3`, `3`, `4`, `2`, `4`, `2`, `4`, `2`, `3`]
   },
   {
-    name: `Tony`,
-    picture: `https://randomuser.me/api/portraits/men/86.jpg`,
-    answers: [`1`, `4`, `4`, `2`, `4`, `1`, `3`, `3`, `3`, `1`]
+    name: "T'Challa",
+    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi85qDuh-_hAhWD4J4KHbx4CvkQjRx6BAgBEAU&url=http%3A%2F%2Fmanjr.com%2F2018%2Fentertainment%2Fcheck-out-the-latest-news-on-marvel%25E2%2580%2599s-black-panther%2F&psig=AOvVaw3XB5KTHLLhHWGWYZgV5LeM&ust=1556412789149288`,
+    answers: [`2`, `4`, `3`, `2`, `1`, `4`, `3`, `4`, `3`, `1`]
   },
   {
-    name: `Natalie`,
-    picture: `https://randomuser.me/api/portraits/women/85.jpg`,
-    answers: [`2`, `5`, `5`, `2`, `3`, `2`, `2`, `3`, `2`, `1`]
+    name: `Okoye`,
+    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjq44fShu_hAhVyNX0KHWMMAIgQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thewrap.com%2Fmarvel-adds-danai-guriras-name-to-avengers-endgame-poster-after-she-was-left-off-the-first-one%2F&psig=AOvVaw3Na7EbQGyE-FfZgZLcxDpE&ust=1556412477086038`,
+    answers: [`2`, `4`, `2`, `3`, `1`, `5`, `1`, `5`, `3`, `1`]
   },
   {
-    name: `Sam`,
-    picture: `https://randomuser.me/api/portraits/men/59.jpg`,
-    answers: [`3`, `2`, `2`, `4`, `2`, `4`, `5`, `4`, `5`, `2`]
+    name: `Killmonger`,
+    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj23K-mhu_hAhV_CjQIHZvvAuYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pride.com%2Fgeek%2F2018%2F9%2F17%2Fkillmonger-getting-his-own-miniseries&psig=AOvVaw03fTsaD6qpsOKj2lXm1DyK&ust=1556412422556448`,
+    answers: [`2`, `4`, `1`, `1`, `5`, `5`, `4`, `3`, `5`, `5`]
   }
 ];
 

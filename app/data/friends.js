74141973dbef1acbbc55cb165e0a74fdb6788623
Julian Hicks <jkballin@gmail.com>
2019-04-27 18:45:06 -0700

    fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 const friendsArray = [
   {
     name: `Nakia`,
-    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq57adiO_hAhUXsJ4KHWquAWQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.zimbio.com%2FThe%2BStars%2Bof%2B%27Black%2BPanther%27%2BIn%2Band%2BOut%2Bof%2BCostume%2Farticles%2F1n2sZ_pd4Gf%2FNakia&psig=AOvVaw00apdAJS0iW5F0s_GpDGt-&ust=1556412941417744`,
+    picture: `https://wearyourvoicemag.com/wp-content/uploads/2018/03/black-panther-lupita-nyong-o.jpg`,
     answers: [`2`, `3`, `3`, `4`, `2`, `4`, `2`, `4`, `2`, `3`]
   },
   {
     name: "T'Challa",
-    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi85qDuh-_hAhWD4J4KHbx4CvkQjRx6BAgBEAU&url=http%3A%2F%2Fmanjr.com%2F2018%2Fentertainment%2Fcheck-out-the-latest-news-on-marvel%25E2%2580%2599s-black-panther%2F&psig=AOvVaw3XB5KTHLLhHWGWYZgV5LeM&ust=1556412789149288`,
+    picture: `https://thumbs-prod.si-cdn.com/Zo5VBilofM_7x3AQxdMzbQCqyKE=/fit-in/1072x0/https://public-media.si-cdn.com/filer/1e/56/1e56f687-ccf4-4f57-a55b-38bf0414c5a3/bp5.jpg`,
     answers: [`2`, `4`, `3`, `2`, `1`, `4`, `3`, `4`, `3`, `1`]
   },
   {
     name: `Okoye`,
-    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjq44fShu_hAhVyNX0KHWMMAIgQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thewrap.com%2Fmarvel-adds-danai-guriras-name-to-avengers-endgame-poster-after-she-was-left-off-the-first-one%2F&psig=AOvVaw3Na7EbQGyE-FfZgZLcxDpE&ust=1556412477086038`,
+    picture: `https://wearyourvoicemag.com/wp-content/uploads/2018/03/black-panther-lupita-nyong-o.jpg`,
     answers: [`2`, `4`, `2`, `3`, `1`, `5`, `1`, `5`, `3`, `1`]
   },
   {
     name: `Killmonger`,
-    picture: `https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj23K-mhu_hAhV_CjQIHZvvAuYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pride.com%2Fgeek%2F2018%2F9%2F17%2Fkillmonger-getting-his-own-miniseries&psig=AOvVaw03fTsaD6qpsOKj2lXm1DyK&ust=1556412422556448`,
+    picture: `https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/michael-b-jordan-killmonger-black-panther-1548776260.jpg?resize=480:*`,
     answers: [`2`, `4`, `1`, `1`, `5`, `5`, `4`, `3`, `5`, `5`]
   }
 ];
 
-module.exports = friendsArray;
+module.exports = { friendsArray };
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,14 @@
 var path = require("path");
 var fs = require('fs');
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname,"../data/friends.js"));
+var { friendsArray } = require('../data/friends');
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
   });
-var friendsArray = [];
 
-  app.post("/api/friends", function(req, res){  
-  // Find the best match before adding them to the app so they aren't matched with themselves
+  app.post("/api/friends", function (req, res) {
+    console.log("input", req.body)
+    // Find the best match before adding them to the app so they aren't matched with themselves
     const matchBest = findFriends(req.body, friendsArray);
     //Cannot add them then compare because there is no unique identifier. There is nothing stopping the user from using the same name or profile pic as another user.
     friendsArray.push(req.body);
@@ -16,6 +17,7 @@ var friendsArray = [];
   //Finding friend with the least difference
   const findFriends = (user, friendsArray) => {
     //Look through all the friends and find a match with the lowest difference
+    console.log("friendsArray", friendsArray);
     let matchBest = {};
     //Difference last is defined outside to persist over each friend
     let lastDiff = 0;
@@ -24,7 +26,7 @@ var friendsArray = [];
       //The answers array comes in a string, converted into a number to find the difference
       friend.answers.forEach(
         (answer, index) =>
-          (answerDiff += Math.abs(+answer - +user.answers[index]))
+          (answerDiff += Math.abs(answer - user.scores[index]))
       );
       if (answerDiff < lastDiff) {
         matchBest = friend;
